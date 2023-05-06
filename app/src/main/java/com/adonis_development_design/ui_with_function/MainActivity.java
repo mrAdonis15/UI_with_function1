54a6b77c9f4108356fd252e4a87ebf93a51f90a6
Your Name <you@example.com>
2023-05-06 19:21:37 +0800

    Initial commit

---
+++ app/src/main/java/com/adonis_development_design/ui_with_function/MainActivity.java
@@ -0,0 +1,14 @@
+package com.adonis_development_design.ui_with_function;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
